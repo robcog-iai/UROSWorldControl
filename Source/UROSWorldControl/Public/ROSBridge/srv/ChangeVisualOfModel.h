55cb952ebfc5418c8daa310eac3dd67fdd87d308
Björn Veit <bveit@uni-bremen.de>
2018-04-25 12:58:41 +0200

    added ChangeVisual functionality (untested, and on hold)

---
+++ Source/UROSWorldControl/Public/ROSBridge/srv/ChangeVisualOfModel.h
@@ -0,0 +1,97 @@
+#pragma once
+
+#include "ROSBridgeSrv.h"
+#include "MeshDescription.h"
+#include "InstanceId.h"
+#include "Pose.h"
+
+
+class UROSBRIDGE_API RosWorldControlChangeVisualOfModelsSrv : public FROSBridgeSrv {
+protected:
+	FString Type;
+
+public:
+	RosWorldControlChangeVisualOfModelsSrv(FString Type_)
+	{
+		Type = Type_;
+	}
+
+	class Request : public SrvRequest {
+	private:
+		unreal_msgs::InstanceId InstanceId;
+		unreal_msgs::MeshDescription MeshDescription;
+
+	public:
+		Request() {}
+		unreal_msgs::InstanceId GetInstanceId() { return InstanceId; };
+
+		unreal_msgs::MeshDescription GetMeshDescription()
+		{
+			return MeshDescription;
+		}
+
+		virtual void FromJson(TSharedPtr<FJsonObject> JsonObject) override
+		{
+			InstanceId.FromJson(JsonObject->GetObjectField("instance_id"));
+			MeshDescription.FromJson(JsonObject->GetObjectField("mesh_description"));
+		}
+
+		static Request GetFromJson(TSharedPtr<FJsonObject> JsonObject)
+		{
+			Request req;
+			req.FromJson(JsonObject);
+			return req;
+		}
+
+		virtual FString ToString() const override
+		{
+			return TEXT("RosWorldControlChangeVisualOfModel::Request {") + InstanceId.ToString() + MeshDescription.ToString() + TEXT("}");
+		}
+
+		virtual TSharedPtr<FJsonObject> ToJsonObject() const
+		{
+			TSharedPtr<FJsonObject> Object = MakeShareable<FJsonObject>(new FJsonObject());
+			Object->SetObjectField("instance_id", InstanceId.ToJsonObject());
+			Object->SetObjectField("mesh_description", MeshDescription.ToJsonObject());
+
+			return Object;
+		}
+
+	};
+
+
+	class Response : public SrvResponse {
+	private:
+		bool bSuccess;
+
+	public:
+		Response() {}
+		Response(bool succeded_) : bSuccess(succeded_) {}
+		bool GetSuccess() const { return bSuccess; }
+		void SetSuccess(bool Success) { bSuccess = Success; }
+
+		virtual void FromJson(TSharedPtr<FJsonObject> JSonObject) override
+		{
+			bSuccess = JSonObject->GetBoolField("success");
+		}
+
+		static Response GetFromJson(TSharedPtr<FJsonObject> JSonObject)
+		{
+			Response resp;
+			resp.FromJson(JSonObject);
+			return resp;
+		}
+
+		virtual FString ToString() const override
+		{
+			return TEXT("RosWorldControlChangeVisualOfModel::Response { %s }"), bSuccess ? TEXT("True") : TEXT("False");
+		}
+
+		virtual TSharedPtr<FJsonObject> ToJsonObject() const
+		{
+			TSharedPtr<FJsonObject> Object = MakeShareable<FJsonObject>(new FJsonObject());
+			Object->SetBoolField("success", bSuccess);
+			return Object;
+		}
+	};
+};
---
+++ Source/UROSWorldControl/Public/ROSBridge/srvServerfiles/ChangeVisualOfModelServer.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "ROSWorldControlManager.h"
+#include "ChangeVisualOfModel.h"
+#include "Engine/StaticMesh.h"
+#include "HelperFunctions.h"
+
+class FROSChangeVisualOfModelServer final : public FROSBridgeSrvServer
+{
+private:
+	FROSChangeVisualOfModelServer();
+	UWorld* World;
+	FThreadSafeBool ServiceSuccess;
+
+	ROSWorldControlManager* Controller;
+
+	bool ChangeVisual(unreal_msgs::MeshDescription MeshDescription, UStaticMeshComponent* MeshComponent);
+
+	void SetServiceSuccess(bool Success);
+
+public:
+	FROSChangeVisualOfModelServer(FString Namespace, FString Name, UWorld* InWorld,
+		ROSWorldControlManager* InController) :
+		FROSBridgeSrvServer(Namespace + TEXT("/") + Name, TEXT("unreal_world_control_msgs/ChangeVisualOfModel")),
+		World(InWorld),
+		Controller(InController)
+	{}
+
+	TSharedPtr<FROSBridgeSrv::SrvRequest> FromJson(TSharedPtr<FJsonObject> JsonObject) const override;
+
+	TSharedPtr<FROSBridgeSrv::SrvResponse> Callback(TSharedPtr<FROSBridgeSrv::SrvRequest> Request) override;
+
+
+};
