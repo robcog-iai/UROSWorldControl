844cde51329715a21d3d6c48ffeeb2ff3b4b5766
Michael Neumann <neumann.michael@uni-bremen.de>
2020-07-09 14:28:39 +0200

    Add checkpoint for AActor

--- Source/UCheckpointSystem/Public/CheckpointHandler.h
+++ Source/UCheckpointSystem/Public/CheckpointHandler.h
@@ -7,7 +7,6 @@
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
 #include "WorldState.h"
-// #include "SaveGameHandler.h"
 #include "CheckpointHandler.generated.h"
 
 /**
@@ -22,27 +21,29 @@ public:
     void Init();
 
         // Save the checkpoint
-    virtual void SaveCheckpoint(const UObject* InSavedObject);
+    // virtual void SaveCheckpoint(const UObject* InSavedObject);
+    virtual void SaveCheckpoint(UObject* InSavedObject);
 
         // Get the checkpoint
-    virtual void ResetCheckpoint(const UObject* OutContent);
+    virtual void ResetCheckpoint(UObject* InSavedObject);
 
 protected:
 
     // Saves the Game. Independent of SaveGame type
     void SaveToFile();
 
+    virtual void ResetCheckpoint();
     // Creates the SaveGame file. Should be overridden by inherited class to generate a
     // specific type of SaveGame
     virtual USaveGame* CreateSaveGameFile();
 
-    // Gets the Content from the SaveObject.
+    // Gets the Content from the SaveObject. Has to be implemented from child class
     virtual void GetContent(){
       UE_LOG(LogTemp, Error, TEXT("GetContent of CheckpointHandler not implemented"));
     };
 
     // Object that contains the information to be saved
-    const UObject* SavedObject;
+    UObject* SavedObject;
 
     FString FileName;
     USaveGame* SaveGame;
@@ -58,6 +59,7 @@ public:
     virtual USaveGame* CreateSaveGameFile() override;
     virtual void GetContent() override;
 
-    virtual void ResetCheckpoint(const UObject* InSavedObject) override;
+    virtual void ResetCheckpoint() override;
+    // virtual void ResetCheckpoint(const UObject* InSavedObject) override;
 
 };
